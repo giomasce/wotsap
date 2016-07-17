e1a0626b8a0ea86a08c5712a4ff29da3c98588f6
Giovanni Mascellani <mascellani@poisson.phc.unipi.it>
2016-07-17 21:25:36 +0200

    Import wotsap 0.6.

--- Webware-Main.py
+++ Webware-Main.py
@@ -20,149 +20,248 @@
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
 # 02111-1307, USA.
 
-dumpfile   = "/web/Webwaredirs/jc/db/dump.wot"
-debiandump = "/web/Webwaredirs/jc/db/dump-debian.wot"
-fontfile   = "/web/Webwaredirs/jc/db/courR14-ISO8859-1.pil"
+dumpfile   = "/web/users/roxen_only/jc/wotsap/wots/latest.wot"
 
-url = "http://webware.lysator.liu.se/jc/wotsap/"
-# It seems like we get URLs in latin-1 (or maybe latin-9, I haven't
-# tried euro sign.)
-urlencoding = 'iso-8859-15'
+url = "http://webware.lysator.liu.se/jc/"
 
-minsize = 50,50
-maxsize = 2000,2000
+minsize     = 200,50
+maxsize     = 2000,2000
+defaultsize = 980,700
+
+maxwanted = 1000
 
 from WebKit.Page import Page
 import sys
-# For some strange reason, I couldn't name the module wotsap. 
-# Something about the context having the same name, I guess.
-import wotsap_module
+import os
+import wotsap
+import re
 
-wot_large  = wotsap_module.Wot(dumpfile,   fontfile)
-wot_debian = wotsap_module.Wot(debiandump, fontfile)
+# _foo=[bar] arguments is an idiom for static variables.
+# An argument gets its default value only once, so if it is mutable it
+# can be changed. The underscore is there to remind the caller that
+# the variable is internal.
+# TODO This should be called once in a while by some sort of timer, to
+# avoid long load times for the first request. Is that possible with
+# Webware?
+def getlatestwot(path=dumpfile, _lastmtime=[0], _wot=[None]):
+    if os.lstat(path).st_mtime != _lastmtime[0]:
+        _lastmtime[0]   = os.lstat(path).st_mtime
+        wot = wotsap.Wot(path)
+        wot.initfont()
+        _wot[0]  = wot
+    return _wot[0]
 
-def findpaths(wot, bottom, top, config, file, setheader):
-	paths = wot.findpaths(bottom, top, config, 'PIL')
-        if not paths:
-            return error(file, "Unable to find path from %s to %s." %\
-			 (bottom, top))
-	setheader( 'Content-type', 'image/png' )
-        paths.save(file, 'png')
-
-def keyinfo(wot, key, file, setheader):
-    info = wot.keystats(key)
-    if info:
-        setheader( 'Content-type', 'text/plain' )
-        file.write(info.encode('iso-8859-15', 'replace'))
+def handlelegacyurl(wot, fields, redirect):
+    """Handle the early, now obsolete, URL scheme."""
+    def fieldtokey(field, regexp="^0x[0-9a-fA-F]{8}$"):
+        if field in fields and  re.compile(regexp).search(fields[field]):
+            return fields[field]
+    bottom, top = fieldtokey('bottom'), fieldtokey('top')
+    if bottom and top:
+        size = fieldtokey('size', '^[0-9]+x[0-9]+$')
+        if size: args = "?size=" + size
+        else:    args = ""
+        redirect(url+"wotsap/wots/latest/paths/%s-%s.png%s"%(bottom,top,args))
+    elif top:
+        redirect(url+"wotsap/wots/latest/keystatistics/%s.txt"%top)
     else:
-        return error(file, "Unable to find key %s." % key)
-        
-def statistics(wot, file, setheader):
-    setheader( 'Content-type', 'text/plain' )
-    file.write(wot.wotstats())
+        redirect("http://www.lysator.liu.se/~jc/wotsap/")
 
-def error(file, str):
-    file.response().setHeader( 'Content-type', 'text/plain' )
-    file.write("error: ")
-    file.write(str.encode('iso-8859-15', 'replace'))
-    file.write("\n")
+def handleurl(urlpath, fields, write, setheader, redirect, urlencode):
+    """Parse URL and take appropriate action."""
+    wot = getlatestwot()
+    # Arguments
+    def checkfields(allowedfields, mandatoryfields=()):
+        ret = ""
+        for field in fields:
+            if field not in mandatoryfields:
+                if field not in allowedfields:
+                    raise ValueError('Unknown field "%s".' % field)
+                if fields[field]:
+                    if not ret: ret += '?'
+                    else:       ret += '&'
+                    ret += '%s=%s' % (urlencode(field),
+                                      urlencode(fields[field]))
+        for field in mandatoryfields:
+            if field not in fields or not fields[field]:
+                raise ValueError('Mandatory field "%s" not specified.' % field)
+        return ret
+    modstring = fields.get('modstring', None)
+    if 'size' in fields and fields['size']:
+        try:
+            size = fields['size'].split('x', 1)
+            size = int(size[0]), int(size[1])
+            if not minsize[0] <= size[0] <= maxsize[0] or \
+               not minsize[1] <= size[1] <= maxsize[1]:
+                raise ValueError(
+                    'Size too large or too small: %s\n'%fields['size'] +\
+                    'It must be between %dx%d and %dx%d, inclusive.\n' % (minsize+maxsize))
+        except IndexError, err:
+            raise ValueError('Error in size "%s". Size must be in format ' \
+                             '"1024x768".' % fields['size'])
+    else:
+        size = defaultsize
+    if 'date' in fields and fields['date']:
+        raise NotImplementedError('Error: date not implemented yet.')
+    # Parse URLpath
+    kre = "0x[0-9a-fA-F]{8}"
+    def matches(regexp):
+        return re.compile(regexp).search(urlpath)
+    # Remove double slashes
+    tmp = -1
+    while tmp != len(urlpath):
+        tmp = len(urlpath)
+        urlpath = urlpath.replace('//', '/')
+    del tmp
+    if urlpath[0:1] == "/":
+        urlpath = urlpath[1:]
+    # Empty URLpath?
+    if   matches("^/*$"):
+        redirect("http://www.lysator.liu.se/~jc/")
+        return
+    # Legacy URLpath?
+    elif matches("^wotsap/$"):
+        if not fields: # We dont support statistics this way anymore.
+            redirect("http://www.lysator.liu.se/~jc/wotsap/")
+        else:
+            return handlelegacyurl(wot, fields, redirect)
+    # Action.
+    elif matches('^wotsap/wots/latest/wotinfo.txt$'):
+        checkfields([])
+        setheader('Content-type', 'text/plain; charset=utf-8' )
+        write(wot.wotstats().encode('utf-8', 'replace'))
+    elif matches('^wotsap/wots/latest/debuginfo.txt$'):
+        checkfields([])
+        setheader('Content-type', 'text/plain; charset=utf-8' )
+        write(wot.debug.encode('utf-8', 'replace'))
+    elif matches('^wotsap/wots/latest/keystatistics/%s\\.txt$'%kre):
+        args = checkfields(['modstring', 'wanted', 'restrict'])
+        wanted   = fields.get('wanted',   0)
+        restrict = fields.get('restrict', None)
+        if wanted:
+            wanted = int(wanted)
+            if wanted > maxwanted:
+                raise ValueError(
+                    'Too large wanted value %d. It must be no larger than %d.'\
+                    % (wanted, maxwanted))
+        key = urlpath[-14:-4]
+        timer = {'time'     : 8,
+                 'fraction' : 0.10,
+                 'action'   : 'raise'}
+        try:
+            stats = wot.keystats(key, modstring=modstring, wanted=wanted,
+                                 restrict=restrict, timer=timer).encode('utf-8', 'replace')
+        except wotsap.wotTimeoutError, err:
+            setheader('Content-type', 'text/plain; charset=utf-8' )
+            err.times['appendstring'] = \
+                     '\nSorry, this is to much. Try restricting to fewer keys or run the stand-alone program.\n'\
+                     'Or look at http://keyserver.kjsl.com/~jharris/ka/current/top50table.html.\n'
+            raise ValueError(str(err))
+        setheader('Content-type', 'text/plain; charset=utf-8' )
+        write(stats)
+    elif matches('^wotsap/wots/latest/paths/%s-%s\\.png$'%(kre,kre)):
+        checkfields(['modstring', 'size'])
+        bottomkey = urlpath[-25:-15]
+        topkey    = urlpath[-14: -4]
+        web   = wot.findpaths(bottomkey, topkey, modstr=modstring)
+        graph = wot.creategraph(web, config={'size': size}, format="PIL")
+        setheader('Content-type', 'image/png')
+        tmpwrite = write
+        class tmpfile:
+            # PIL documentation says we need seek, tell, and write,
+            # but write seems to be sufficient.
+            write = tmpwrite
+        graph.save(tmpfile, 'png')
+    elif matches('^wotsap/wots/latest/groupmatrix/%s(,%s)*\\.txt$'%(kre,kre)):
+        checkfields(['modstring'])
+        keys = urlpath[31:-4]
+        setheader('Content-type', 'text/plain; charset=utf-8')
+        for line in wot.groupmatrix(keys, modstring=modstring):
+            write(line.encode('utf-8', 'replace') + '\n')
+    # Searches
+    # raise wot.wotKeyNotFoundError(str2key(path[14:24]))
+    elif matches('^wotsap/search/wotinfo$'):
+        checkfields([])
+        redirect(url+"wotsap/wots/latest/wotinfo.txt")
+    elif matches('^wotsap/search/debuginfo$'):
+        checkfields([])
+        redirect(url+"wotsap/wots/latest/debuginfo.txt")
+    elif matches('^wotsap/search/keystatistics$'):
+        args = checkfields(['modstring', 'wanted', 'restrict'], ['key'])
+        key = wot.nametokey(fields['key'])
+        redirect(url+"wotsap/wots/latest/keystatistics/%s.txt"%key +args)
+    elif matches('^wotsap/search/paths$'):
+        args = checkfields(['modstring', 'size'], ['bottom', 'top'])
+        bottom = wot.nametokey(fields['bottom'])
+        top    = wot.nametokey(fields['top'   ])
+        redirect(url+"wotsap/wots/latest/paths/%s-%s.png"%(bottom,top) +args)
+    elif matches('^wotsap/search/groupmatrix$'):
+        args = checkfields(['modstring'], ['keys'])
+        namelist = fields['keys'].split(',')
+        if len(namelist) > 1:
+            keys = ','.join([wot.nametokey(name) for name in namelist])
+            redirect(url+"wotsap/wots/latest/groupmatrix/%s.txt"%keys +args)
+        else:
+            setheader('Content-type', 'text/plain; charset=utf-8')
+            for line in wot.groupmatrix(None, searchstring=namelist[0],
+                                        modstring=modstring):
+                write(line.encode('utf-8', 'replace') + '\n')
+    elif matches('^wotsap/search/listkeys$'):
+        args = checkfields([], ['keys'])
+        setheader('Content-type', 'text/plain; charset=utf-8')
+        for name in wot.listkeys(fields['keys']):
+            write(name.encode('utf-8', 'replace') + '\n')
+    else: # Unknown URLpath
+        redirect("http://www.lysator.liu.se/~jc/wotsap/")
+        #setheader('Content-type', 'text/plain; charset=utf-8')
+        #write("Unknown path: ")
+        #write(urlpath.encode('utf-8', 'replace') + '\n')
+        #write(str(fields) + '\n')
 
-# Filter and sort arguments
-# This isn't really necessary, but if people are going to start
-# linking to images, they will run into problems when the list order
-# and moon phase changes, to their search terms matches another key.
-def redirect(fields, sendredirect, urlencode):
-    str = url + "?"
-    for arg in ["top", "bottom", "size",
-                "arrowlen", "arrowang", "colors", "wot"]:
-        if fields.get(arg, None):
-            str += arg + "="+urlencode(fields[arg])+"&"
-    str = str[:-1]
-    sendredirect(str)
+def errorwrapper(urlpath, fields, write, setheader, sendredirect, urlencode):
+    try:
+        return handleurl(urlpath, fields, write, setheader, sendredirect, urlencode)
+    except (ValueError, NotImplementedError, wotsap.wotTooManyError,
+            wotsap.wotKeyNotFoundError, wotsap.wotModstringError), err:
+        setheader('Content-type', 'text/plain; charset=utf-8')
+        write(unicode(err).encode('utf-8', 'replace'))
 
-
+# Every time someone requests a page, Webware calls Main.writeHTML. 
+# Webware is kind enough to give us both the whole URL between
+# "http://webware.lysator.liu.se/jc/" and "?", and both GET and POST
+# form entries nicely interpreted and put into a dictionary. All we
+# have to do is parse these and take appropriate action.
 class Main(Page):
-
     def writeHTML(self):
         request  = self.request()
-        response = self.response()
-        fields   = request.fields()
+        sendredirect = self.response().sendRedirect
+        setheader    = self.response().setHeader
+        urlencode    = self.urlEncode
+        fields       = self.request().fields()
+        urlpath      = self.request().originalURLPath() 
+        write        = self.write
 
-        wot = fields.get("wot", "large")
-        if wot == "debian":
-            wot = wot_debian
-        else:
-            wot = wot_large
-
-
-        for arg in ["bottom", "top"]:
-            if fields.has_key(arg):
-                fields[arg] = unicode(fields[arg], urlencoding)
-
-        bottom = fields.get("bottom", None)
-        top    = fields.get("top",    None)
-
-        keybottom=keytop=None
-        if bottom:
-            keybottom = wot.nametokey(bottom)
-            if not keybottom:
-                return error(self, "Unable to find key matching %s." % bottom)
-        if top:
-            keytop    = wot.nametokey(top)
-            if not keytop:
-                return error(self, "Unable to find key matching %s." % top)
-        if bottom != keybottom or top != keytop:
-            fields["bottom"] = keybottom
-            fields["top"]    = keytop
-            return redirect(fields, response.sendRedirect, self.urlEncode)
-
-        config = {}
-
-        if fields.has_key("size"):
-            sizestr = fields["size"]
-            size    = sizestr.split('x')
-            if len(size) != 2:
-                return error(self, 'Size must be in format "1024x768".')
-            try:
-                size = (int(size[0]), int(size[1]))
-            except ValueError:
-                return error(self, 'Size must be in format "1024x768".')
-            if not minsize[0] <= size[0] <= maxsize[0] or not\
-                   minsize[1] <= size[1] <= maxsize[1]:
-                return error(self, 'Size too large or too small: %s'%sizestr)
-            config["size"] = size
-
-        try:
-	    for f in ["arrowlen", "arrowang"]:
-	        if fields.has_key(f):
-                    config[f] = int(fields[f])
-        except ValueError:
-            return error(self, "Illegal integer %s." % fields[f])
-
-        if fields.has_key("colors"):
-            s = fields["colors"]
-            colors = []
-            try:
-                if len(s) > wot.avail_colors*6:
-                    raise ValueError
-                while len(s) >= 6:
-                    colors.append(int(s[0:2], 16))
-                    colors.append(int(s[2:4], 16))
-                    colors.append(int(s[4:6], 16))
-                    s = s[6:]
-                if len(s):
-                    raise ValueError
-            except ValueError:
-                return error(self, "Illegal colormap: %s\n" % s +\
-			     "Colormap should be specified as a number "+\
-			     "of 3-byte (RGB) colors, written as\n"+\
-			     "6 hexadecimal characters.\n")
-            config["colors"] = colors
+        # Useful when debugging:
+        #def sendredirect(url):
+        #    setheader('Content-type', 'text/plain; charset=utf-8')
+        #    write('Intercepted redirect to: %s' % url)
         
-        if top and bottom:
-            return findpaths(wot, bottom, top, config,
-                             self, response.setHeader)
-        if top:
-            return keyinfo(wot, top, self, response.setHeader)
-
-        return statistics(wot, self, response.setHeader)
+        # The strings we get from Webware are just the 7bit URL
+        # decoded to 8bit octet streams. HTML4.01 says that that
+        # stream is latin1 encoded. In practice, things are very
+        # complicated. See e.g.
+        # http://ppewww.ph.gla.ac.uk/~flavell/charset/form-i18n.html
+        # . It seems to indicate that browsers often choose the same
+        # encoding as the web page the form is on. If we code the web
+        # page in iso-8859-1, both that rule and the HTML4.01 spec
+        # says that the string should be iso-8859-1. This will make it
+        # impossible to enter other characters. The only other simple
+        # alternative would be to have the web page coded in UTF-8 and
+        # treat all form data as UTF-8, but that would mean that
+        # standards-conforming browsers wouldn't be able to send
+        # non-ASCII iso-8859-1 characters. Tons of other heuristics
+        # exists if someone wants to make it more stable.
+        urlpath = urlpath.decode('iso-8859-1', 'replace')
+        for key in fields: # The field names themselves are ascii.
+            fields[key] = fields[key].decode('iso-8859-1', 'replace')
+        return errorwrapper(urlpath, fields, write, setheader, sendredirect, urlencode)
